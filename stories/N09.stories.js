8c41f3055f4b72e588904953b1afab09571a4a25
p0k3m0n2k1 <p0k3m0n2k1@gmail.com>
2018-06-11 00:11:47 +0700

    new commit  Please enter the commit message for your changes. Lines starting

--- stories/N09.stories.js
+++ stories/N09.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Board from "../N09/index.jsx";
-import Game from "../N09/lib/N09.js";
+import Board from "../src/N09/index.jsx";
+import Game from "../src/N09/lib/N09.js";
 import ReactGame from "react-gameboard/lib/component";
 
 const TOH = ReactGame(Game);

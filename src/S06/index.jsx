8aab73fa38e5984e9a9bdc60103af7b9771ce09d
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-06-10 11:37:40 +0700

    Fix available bugs

--- src/S06/index.jsx
+++ src/S06/index.jsx
@@ -4,7 +4,16 @@ import "./index.less";
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick} disabled={props.isOver}>
+    <button
+      className="square"
+      onClick={props.onClick}
+      disabled={props.isOver}
+      style={{
+        color: props.isChoosen ? "white" : "#00b2cc",
+        backgroundColor: props.isChoosen ? "#00e5ff" : "white",
+        borderColor: "#00b2cc"
+      }}
+    >
       {props.value}
     </button>
   );
@@ -13,10 +22,11 @@ function Square(props) {
 class Board extends React.Component {
   render() {
     const board = this.props.state.board;
+    const selection = this.props.state.selection;
     const N = board.length;
     const M = board[0].length;
 
-    const err = this.props.error ? this.props.error.message : null;
+    const error = this.props.error ? this.props.error.message : null;
 
     const array = [];
     for (let j = 0; j < M; ++j) {
@@ -27,6 +37,8 @@ class Board extends React.Component {
             key={"data" + i + "-" + j}
             isOver={this.props.isEnding}
             onClick={() => this.props.choose({ x: i, y: j })}
+            value={board[i][j]}
+            isChoosen={selection[i][j]}
           />
         );
       array.push(
@@ -36,7 +48,18 @@ class Board extends React.Component {
       );
     }
 
-    return <div className="s06">{array}</div>;
+    return (
+      <div className="s06">
+        {array}
+        <h1 style={{ color: "red" }} className="msg">
+          {error}
+        </h1>
+
+        <h1 style={{ color: "green" }} className="msg">
+          {this.props.isEnding === "won" ? "YOU WON" : ""}
+        </h1>
+      </div>
+    );
   }
 }
 
--- src/S06/lib/tshirt.js
+++ src/S06/lib/tshirt.js
@@ -1,25 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 
-const dx = [-1, 0, 0, 1];
-const dy = [0, -1, 1, 0];
-
 function check(x, y, m, n) {
   if (x >= 0 && x < n && y >= 0 && y < m) return true;
   return false;
 }
 
-function shift(og) {
-  if (!(og instanceof Array)) return null;
-  let k = og;
-  let t = k.pop();
-  k.unshift(t);
-  return k;
-}
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let x = a[i];
+    const j = Math.floor(Math.random() * (i + 1));
+    const x = a[i];
     a[i] = a[j];
     a[j] = x;
   }
@@ -33,57 +23,16 @@ const Tshirt = {
     const n = props.n;
     let board = [];
 
-    //If n < m, we can easily do this (circular message and shuffle). Else, swap them, then just
-    //print them as if they are n, m respectively.
-    //Insert elements randomly inside our first row
-    let row = [];
-    for (let i = 1; i <= m; ++i) row.push(i);
-    row = shuffle(row);
+    let first = Array.from(new Array(m), (val, index) => index + 1);
+    first = shuffle(first);
 
-    //Make all rows by shifting them to the right once.
-    let all = [];
     for (let i = 0; i < n; ++i) {
-      all.push(row);
-      row = shift(row);
+      let a = first.map(v => v);
+      board.push(a);
+      first.unshift(first.pop());
     }
-
-    //Shuffle them. Same as above, idea is to save all possible positions, pick a random one then
-    //erase it from the vector
-    let ava = [];
-    for (let i = 0; i < n; ++i) ava.push(i);
-    ava = shuffle(ava);
-
-    for (let cur = 0; cur < n; ++cur) board.push(all[ava[cur]]);
-
-    //mark stores which spot are considered "broken", if our spot will be broken we store it as
-    //a random board[i][k] that k < j. Put it first to board[i][j - 1] to ensure it will be changed to at least
-    //1 possible element
-    let mark = [...Array(n)].map(e => Array(m).fill(false));
-    for (let i = 0; i < n; ++i) {
-      for (let j = 1; j < m; ++j) {
-        const era = Math.floor(Math.random() * 5);
-        if (era < 2) {
-          let cant = false;
-          for (let k = 0; k < 4; ++k)
-            if (check(i + dx[k], j + dy[k], m, n) && mark[i + dx[k]][j + dy[k]])
-              cant = true;
-          if (!cant) {
-            board[i][j] = Math.max(
-              board[i][j - 1],
-              i !== 0 ? board[i - 1][j] : 0
-            );
-            for (let k = 0; k < j - 1; ++k) {
-              const putthis = Math.floor(Math.random() * 2);
-              if (putthis === 1) {
-                board[i][j] = board[i][k];
-                break;
-              }
-            }
-            mark[i][j] = null;
-          }
-        }
-      }
-    }
+    board = shuffle(board);
+    console.table(board);
 
     const selection = [...Array(n)].map(e => Array(m).fill(false));
 
@@ -95,6 +44,18 @@ const Tshirt = {
       let selection = state.selection;
       const n = board.length;
       const m = board[0].length;
+      const dx = [-1, 0, 0, 1];
+      const dy = [0, -1, 1, 0];
+
+      let cant = false;
+      for (let k = 0; k < 4; ++k) {
+        if (
+          check(x + dx[k], y + dy[k], m, n) &&
+          selection[x + dx[k]][y + dy[k]]
+        )
+          throw new Error("Marked number can't be lined up");
+      }
+
       selection[x][y] = !selection[x][y];
       return { board, selection };
     },

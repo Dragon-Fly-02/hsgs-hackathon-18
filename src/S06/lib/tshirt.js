2e53b4fe7dee566ebbe2dbf634d01d806d2a3cbd
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-06-10 09:49:59 +0700

    Fix display problem

--- src/S06/lib/tshirt.js
+++ src/S06/lib/tshirt.js
@@ -16,28 +16,32 @@ function shift(og) {
   k.unshift(t);
   return k;
 }
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
 
 const Tshirt = {
   default(props = { m: 6, n: 6 }) {
     // n cot, m hang
     const m = props.m;
     const n = props.n;
-    let board = [...Array(n)].map(e => Array(m).fill(null));
+    let board = [];
 
     //If n < m, we can easily do this (circular message and shuffle). Else, swap them, then just
     //print them as if they are n, m respectively.
-    let vector = [];
-    for (let i = 1; i <= n; ++i) vector.push(i);
-
     //Insert elements randomly inside our first row
     let row = [];
-    let all = [];
-    while (row.length < m) {
-      const temp = Math.floor(Math.random() * vector.length);
-      row.push(vector[temp]);
-      vector.splice(temp);
-    }
+    for (let i = 1; i <= m; ++i) row.push(i);
+    row = shuffle(row);
+
     //Make all rows by shifting them to the right once.
+    let all = [];
     for (let i = 0; i < n; ++i) {
       all.push(row);
       row = shift(row);
@@ -47,13 +51,9 @@ const Tshirt = {
     //erase it from the vector
     let ava = [];
     for (let i = 0; i < n; ++i) ava.push(i);
-    let cur = n;
-    while (cur > 0) {
-      const temp = Math.floor(Math.random() * ava.length);
-      board.push(all[ava[temp]]);
-      ava.splice(temp);
-      --cur;
-    }
+    ava = shuffle(ava);
+
+    for (let cur = 0; cur < n; ++cur) board.push(all[ava[cur]]);
 
     //mark stores which spot are considered "broken", if our spot will be broken we store it as
     //a random board[i][k] that k < j. Put it first to board[i][j - 1] to ensure it will be changed to at least
@@ -61,7 +61,7 @@ const Tshirt = {
     let mark = [...Array(n)].map(e => Array(m).fill(false));
     for (let i = 0; i < n; ++i) {
       for (let j = 1; j < m; ++j) {
-        const era = Math.floor(Math.random() * vector.length);
+        const era = Math.floor(Math.random() * 5);
         if (era < 2) {
           let cant = false;
           for (let k = 0; k < 4; ++k)

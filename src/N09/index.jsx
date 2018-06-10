9079e61a9df05c3ef2ac8b119b13b494c15ecfe4
p0k3m0n2k1 <p0k3m0n2k1@gmail.com>
2018-06-11 00:56:40 +0700

    add .less and minor updates

--- src/N09/index.jsx
+++ src/N09/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import N09 from "./lib/N09.js";
-import "./index.css";
+import "./index.less";
 
 class Equation extends React.Component {
 	render() {
@@ -11,24 +11,26 @@ class Equation extends React.Component {
 			boxes.push([<button onClick={() => this.props.changeo({pos: i})}>{operations[this.props.oper[i]%4]+this.props.num[i]}</button>]);
 			boxes.push([<div>{'----->'+this.props.res[i]}</div>]);
 		}
-		if(this.props.res[N-1]==this.props.num[N]) boxes.push([<div>CONGRATZ, YOU HAVE DONE IT!!!</div>])
+		if(this.props.res[N-1]==this.props.num[N]) boxes.push([<div>Chúc mừng, bạn đã thành công !!! 👏🏼👏🏼👏🏼</div>])
 		return <div>{boxes}</div>
 	}
 }
 
-class Board extends React.Component {
+class Paragraph extends React.Component {
 	render() {
 		var num=this.props.state.num, oper=this.props.state.oper, N=this.props.state.N, res=this.props.state.res;
 		var changeo=this.props.changeo.bind(this);
 		var operations=['+','-','*','/'];
 		var err=this.props.error ? this.props.error.message : 'NULL';
 		return (
-			<div>
-				<div class="container">
+			<div class="N09">
+				<div>
 					<h1>
-					Adjust number of numbers
-					<button onClick={() => this.props.inN()} style={{margin: "auto"}}>+</button>
-					<button onClick={() => this.props.deN()} style={{margin: "auto"}}>-</button>
+					Dùng các phép tính cộng, trừ, nhân, chia để tạo ra được kết quả cuối cùng là {num[N-1]}.
+					Lưu ý: Khi điều chỉnh số số hạng thì sẽ tạo 1 game mới! <br/>
+					Điều chỉnh số số hạng có trong phép tính => 
+					<button onClick={() => this.props.inN()} style={{margin: "0px 10px 0px 10px", width: "30px"}}>+</button>
+					<button onClick={() => this.props.deN()} style={{margin: "auto",  width: "30px"}}>-</button>
 					<Equation num={num} oper={oper} N={N} changeo={changeo} res={res}/>
 					</h1>
 				</div>
@@ -39,4 +41,4 @@ class Board extends React.Component {
 	}
 }
 
-export default Board;
+export default Paragraph;

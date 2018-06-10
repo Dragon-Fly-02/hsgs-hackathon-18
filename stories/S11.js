084c974f2aed4e7e17ae6674786cdb620c8286b3
Natsu Kagami <natsukagami@gmail.com>
2018-06-10 13:16:37 +0700

    S11: convert CSS to LESS

--- stories/S11.js
+++ stories/S11.js
@@ -2,9 +2,9 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Board from "../src/S11/index.jsx";
 import Game from "../src/S11/lib/S11.js";
-import {ReactGame} from "react-gameboard";
+import { ReactGame } from "react-gameboard";
 import { number, withKnobs } from "@storybook/addon-knobs";
-import '../src/S11/index.css';
+import "../src/S11/index.less";
 
 const S11 = ReactGame(Game);
 
@@ -12,26 +12,31 @@ storiesOf("Aome vô địch", module)
   .addDecorator(withKnobs)
   .add("How to play", () => {
     return (
-      <div className="divs">
-<div>
-    Ở một ngôi nhà nọ có N Aome sống chung, họ có tên lần lượt là Aome 1, Aome 2, ... Aome N. 
-    Sau nhiều ngày ăn chơi, những Aome đã vay nợ nhau một cách lộn xộn. 
-    Nhiệm vụ của bạn là dùng số lần trả nợ ít nhất sao cho không ai còn nợ tiền.
-</div>
-<div>
-    Việc nợ tiền của các Aome được biểu diễn như một cái bảng với ô (i, j) có giá trị X nghĩa là Aome i nợ Aome j X đồng. 
-</div>
-<div>
-    Một hành động trả nợ được biểu diễn gồm 3 giá trị (A, B, X) nghĩa là Aome A sẽ trả Aome B X đồng.
-</div>
-<div> 
-    Một bước trả nợ bạn có thể điền A vào sau First person, B vào sau Second Person, X vào sau Balance rồi bấm Submit
-</div>
-<div>
-    Tùy vào việc đáp án của bạn có tối ưu hay không mà bạn sẽ nhận được từ 1 ⋆ đến 3 ⋆.
-</div>
-</div>
-    )
+      <div className="divs s11">
+        <div>
+          Ở một ngôi nhà nọ có N Aome sống chung, họ có tên lần lượt là Aome 1,
+          Aome 2, ... Aome N. Sau nhiều ngày ăn chơi, những Aome đã vay nợ nhau
+          một cách lộn xộn. Nhiệm vụ của bạn là dùng số lần trả nợ ít nhất sao
+          cho không ai còn nợ tiền.
+        </div>
+        <div>
+          Việc nợ tiền của các Aome được biểu diễn như một cái bảng với ô (i, j)
+          có giá trị X nghĩa là Aome i nợ Aome j X đồng.
+        </div>
+        <div>
+          Một hành động trả nợ được biểu diễn gồm 3 giá trị (A, B, X) nghĩa là
+          Aome A sẽ trả Aome B X đồng.
+        </div>
+        <div>
+          Một bước trả nợ bạn có thể điền A vào sau First person, B vào sau
+          Second Person, X vào sau Balance rồi bấm Submit
+        </div>
+        <div>
+          Tùy vào việc đáp án của bạn có tối ưu hay không mà bạn sẽ nhận được từ
+          1 ⋆ đến 3 ⋆.
+        </div>
+      </div>
+    );
   })
   .add("with default props", () => (
     <S11 _N={4}>
@@ -51,4 +56,3 @@ storiesOf("Aome vô địch", module)
       </S11>
     );
   });
-

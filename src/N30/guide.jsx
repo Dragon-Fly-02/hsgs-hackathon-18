14da870e9bd3977f7d7950240905ed1e696075e8
cheater2k <vink@Vinh_Dell.localdomain>
2018-06-11 16:51:25 +0700

    Added N13. Modified

--- src/N30/guide.jsx
+++ src/N30/guide.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import "./guide.less"
 
 class Guide extends React.Component {
     render() {
         return(
-            <body>
-                <h1> Hướng dẫn sử dụng game N30. </h1>
+            <div className="guide">
+                <h1> Hướng dẫn chơi </h1>
 
-                <p>
-                   Cho 1 bảng ô vuông. <br/>
-                   Mỗi con số trong bảng đại diện cho số lượng chính xác các ô đen quanh nó. <br/>
-                   (Ở đây ta định nghĩa các ô xung quanh 1 ô là những ô kề cạnh). <br/>
-                   Hay tô đen các ô trên bảng theo đúng quy luật trên. <br/>
+                <span>
+                   Cho 1 bảng ô vuông.
+                   Mỗi con số trong bảng đại diện cho số lượng chính xác các ô đen quanh nó.
+                   (Ở đây ta định nghĩa các ô xung quanh 1 ô là những ô kề cạnh).
+                   Hay tô đen các ô trên bảng theo đúng quy luật trên.
                    (Chú ý được tô đen cả ô chứa số).
-                </p>
+                </span>
 
-                <p>
-                    Để đổi màu 1 ô trên bảng, nháy chuột vào ô cần đổi. <br/>
-                    Ô được nháy sẽ đổi màu từ trắng sang đen hoặc từ đen sang trắng. <br/>
-                    Với các ô chứa số, tại mỗi thời điểm ô đó sẽ thể hiện số lượng chính xác các ô đen xung quanh nó chưa được tô. <br/>
-                </p>
-            </body>
+                <span>
+                    Để đổi màu 1 ô trên bảng, nháy chuột vào ô cần đổi.
+                    Ô được nháy sẽ đổi màu từ trắng sang đen hoặc từ đen sang trắng.
+                    Với các ô chứa số, tại mỗi thời điểm ô đó sẽ thể hiện số lượng chính xác các ô đen xung quanh nó chưa được tô.
+                </span>
+            </div>
         );
     }
 }

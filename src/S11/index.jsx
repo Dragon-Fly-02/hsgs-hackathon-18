a86b14b4e1ea800d696c2048cb38f6eb1a83cca7
Natsu Kagami <natsukagami@gmail.com>
2018-06-11 10:49:54 +0700

    Fixed conflicts: I forget to save T_T

--- src/S11/index.jsx
+++ src/S11/index.jsx
@@ -20,11 +20,7 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
       name: "A",
-=======
-      name: "Aome",
->>>>>>> origin/master
       a: 0,
       b: 0,
       val: 0
